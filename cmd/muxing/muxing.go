b7d7ce3159507f477e61a0f147f81419b2f435af
samatt90 <samatt90@gmail.com>
2022-07-15 17:18:47 +0600

    init: func

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,9 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", handleSuccess).Methods(http.MethodGet)
+	router.HandleFunc("/bad", handleBad).Methods(http.MethodGet)
+	router.HandleFunc("/data", handleData).Methods(http.MethodPost)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -29,10 +33,29 @@ func Start(host string, port int) {
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
 func main() {
-	host := os.Getenv("HOST")
+	// host := os.Getenv("HOST")
+	host := os.Getenv("localhost")
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 	if err != nil {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func handleSuccess(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, PARAM!")
+	return
+}
+
+func handleBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(d)
 }
